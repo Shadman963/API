4094215f4ff4d0bc03e40064e0ce2b6449c28eb9
Sadman <shadmansakib2469@gmail.com>
2026-06-23 23:57:28 +0600

    First Commit

---
+++ mysite/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("blogpost/",views.BlogPostListCreate.as_view(),name="blogpost-view-create"),
+    path("blogpost/<int:pk>/",
+         views.BlogPostRetrieveUpdateDestroy.as_view(),
+         name="update")
+]
+    
+
